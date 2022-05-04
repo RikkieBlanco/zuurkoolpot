df32392cb3c0ca625050f85c973efe83909406a0
Rikkert <rikweel@gmail.com>
2022-05-04 16:43:09 +0200

    hamburger interactiveness

--- menuPopOut.js
+++ menuPopOut.js
@@ -2,5 +2,6 @@ const hamburger = document.querySelector(".hamburger");
 const dropDown = document.querySelector(".dropdown");
 
 hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
     dropDown.classList.toggle("active");
 })
