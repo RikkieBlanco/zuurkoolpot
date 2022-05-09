78ad51b4489c93cf19890eaf1f89acba083c58f2
Rikkert <rikweel@gmail.com>
2022-05-09 21:24:56 +0200

    add button functionality for modal pop up

--- menuPopOut.js
+++ menuPopOut.js
@@ -5,3 +5,25 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     dropDown.classList.toggle("active");
 })
+
+// modal items
+
+const modal = document.getElementById('email-modal');
+const openBtn = document.querySelector('.lijst-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+// click events
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
+
+window.addEventListener('click', (e) => {
+    if(e.target === modal) {
+        modal.style.display = 'none';
+    }
+})
